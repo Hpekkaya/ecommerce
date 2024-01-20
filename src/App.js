679be446076d6d3636c8b87a02e0a9e47644ec07
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-20 12:13:25 +0300

    Main structure generated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Contact from './pages/contact/Contact'
 
 const App = () => {
   return (
-    <div>
-      Wellcome to E-Commerce
-    </div>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   )
 }
 
