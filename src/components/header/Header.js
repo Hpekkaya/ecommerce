a58abab6bd9d55a62323062cc626089b355e8fed
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-20 12:31:31 +0300

    Logo added to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,24 @@
-//// tüm linklerin olduğu başlık kısmı
+// Header section with all links
 import React from 'react'
+import styles from "./Header.module.scss"
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const logo = (
+    <div className={styles.logo}>
+      <Link to="/">
+        <h2>
+          e<span>Shop</span>
+        </h2>
+      </Link>
+    </div>
+  )
   return (
-    <div>Header</div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+      </div>
+    </header>
   )
 }
 
