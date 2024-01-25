3aa31f6c962429e741e725eb5dee66cb7a87da9d
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-25 11:10:39 +0300

    Card structure and styles applied

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,29 +4,31 @@ import styles from "./auth.module.scss"
 import loginImg from "../../assets/login.png"
 import { Link } from 'react-router-dom'
 import { FaGoogle } from 'react-icons/fa'
+import Card from '../../components/card/Card'
 
 const Login = () => {
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
-        <img src={loginImg} alt="Login" width="400"/>
+        <img src={loginImg} alt="LoginImage" width="400"/>
       </div>
-      <div className={styles.form}>
+      <Card cardClass={styles.form}>
         <h2>Login</h2>
         <form>
           <input type="text" placeholder="eMail" required/>
           <input type="password" placeholder="Password" required/>
-          <button className="--btn -btn-primary --btn-block">Login</button>
+          <button className='--btn --btn-primary --btn-block'>Login</button>
           <div className={styles.links}>
-            <Link to="/reset">Reset Pasword</Link>
+            <Link to="/reset">Reset Password</Link>
           </div>
           <p>-- or --</p>
         </form>
-        <button className='=--btn --btn-danger --btn-block'><FaGoogle color='#ffff'/>&nbsp; Login with Google</button>
-        <span className=''>
-
+        <button className='--btn --btn-danger --btn-block'><FaGoogle color='#ffff'/>&nbsp; Login with Google</button>
+        <span className={styles.register}>
+          <p>Don't have an account? </p>
+          <Link to="/register">&nbsp;Register</Link>
         </span>
-      </div>
+      </Card>
     </section>
   )
 }
