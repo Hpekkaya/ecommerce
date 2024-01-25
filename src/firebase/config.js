75305a9bca4b8a788264ac996f44c479a21b169e
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-25 14:39:17 +0300

    Firebese created

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,19 @@
-//// burada google firebase ile etkileşime buradan girilecek.
+// burada google firebase ile etkileşime buradan girilecek.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDsp4OJKjeTcd9W2vARIFTT0aOeqBsuP0",
+  authDomain: "eshop01-7bf83.firebaseapp.com",
+  projectId: "eshop01-7bf83",
+  storageBucket: "eshop01-7bf83.appspot.com",
+  messagingSenderId: "210411876533",
+  appId: "1:210411876533:web:0560b4842c4f0dc5e34efc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
