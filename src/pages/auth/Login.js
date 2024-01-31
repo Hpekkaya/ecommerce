19a3d8e71d217165981d7bdf74bb8f7f32df2a6c
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-31 14:46:16 +0300

    signIn WithEmailAndPassword function added

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,23 +1,57 @@
 // Login Page for registered users
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.scss"
 import loginImg from "../../assets/login.png"
 import { Link } from 'react-router-dom'
 import { FaGoogle } from 'react-icons/fa'
 import Card from '../../components/card/Card'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/config' 
 
 const Login = () => {
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+  
+  const loginUser = (e) => {
+    e.preventDefault();
+    // console.log(email, password);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
   return (
+
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
         <img src={loginImg} alt="LoginImage" width="400"/>
       </div>
       <Card cardClass={styles.form}>
         <h2>Login</h2>
-        <form>
-          <input type="text" placeholder="eMail" required/>
-          <input type="password" placeholder="Password" required/>
+        <form onSubmit={loginUser}>
+        <input
+              type="text"
+              placeholder="eMail"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           <button className='--btn --btn-primary --btn-block'>Login</button>
           <div className={styles.links}>
             <Link to="/reset">Reset Password</Link>
