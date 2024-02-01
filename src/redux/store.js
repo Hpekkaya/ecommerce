35127a5f92ff94611fe12a9d4cc5810cb5f6a0b8
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-01 17:30:30 +0300

    Provider crated in index.js and Set_Active_User created in Header

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,14 @@
-//// burası redux store kayıt yeri
+// Redux store page
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./slice/authSlice"
+
+const rootReducer = combineReducers(){
+
+}
+
+const store = configureStore({
+    reducer:rootReducer
+})
+
+export default store
