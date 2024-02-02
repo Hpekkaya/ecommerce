f66e243f6dc28762175fa874ccadbdcdf16a6d38
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-02 10:08:59 +0300

    sign In With Google function reviewed

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -44,22 +44,16 @@ const Login = () => {
       .then((result) => {
         toast.success("Login with Google is succesful..")
         navigate("/")
-        // // This gives you a Google Access Token. You can use it to access the Google API.
+        // This gives you a Google Access Token. You can use it to access the Google API.
         // const credential = GoogleAuthProvider.credentialFromResult(result);
         // const token = credential.accessToken;
-        // // The signed-in user info.
+        // The signed-in user info.
         // const user = result.user;
-        // // IdP data available using getAdditionalUserInfo(result)
+        // IdP data available using getAdditionalUserInfo(result)
       })
       .catch((error) => {
         // Handle Errors here.
         toast.error(error.message)
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // // The email of the user's account used.
-        // const email = error.customData.email;
-        // // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
       });
   };
 
