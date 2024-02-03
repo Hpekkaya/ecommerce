38bd393eec981a31f00e7724f7a5e337fbe7148a
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-03 23:51:11 +0300

    Slide next and prev func added

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -7,37 +7,48 @@ import { Slide } from 'react-toastify'
 
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const slideLength = sliderData.length;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
   return (
-    <div className='slider'>
-      <AiOutlineArrowLeft className="arrow prev"/>
-      <AiOutlineArrowRight className="arrow next"/>
+    <div className="slider">
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide}/>
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide}/>
 
-      {
-        sliderData.map((slide,index) => {
-          const {image,heading,desc} = slide
-          return (
-            <div key={index} className={index === currentSlide ? "slide current" : "slide"}>
-                {
-                  index === currentSlide && (
-                    <>
-                    <img src={image} alt="slide"/>
-                    <div className='content'>
-                      <h2>{heading}</h2>
-                      <p>{desc}</p>
-                      <hr/>
-                      <a href="#product" className='--btn --btn-primary'>Shop Now</a>
-                    </div>
-                    </>
-                  )
-                }
-            </div>
-          )
-        })
-      }
+      {sliderData.map((slide, index) => {
+        const { image, heading, desc } = slide;
+        return (
+          <div
+            key={index}
+            className={index === currentSlide ? "slide current" : "slide"}
+          >
+            {index === currentSlide && (
+              <>
+                <img src={image} alt="slide" />
+                <div className="content">
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <a href="#product" className="--btn --btn-primary">
+                    Shop Now
+                  </a>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Slider
