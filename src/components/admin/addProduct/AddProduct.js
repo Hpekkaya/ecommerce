3ef5b5ec7600c4bbed55f585ed4f9e5994336af1
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-07 23:28:41 +0300

    Product Name added to AddProducts page

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -1,11 +1,58 @@
 // admin panelindeki add product componenti
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./AddProduct.module.scss"
 
 
+const categories = [
+  {id:1, name:"Laptop"},
+  {id:2, name:"Electronics"},
+  {id:3, name:"Fashion"},
+  {id:4, name:"Phone"}
+]
+
+// 
 const AddProduct = () => {
+
+  const [product, setProduct] = useState({
+    name :"",
+    imageURL :"",
+    price :"",
+    category :"",
+    brand :"",
+    desc :""
+  })
+
+   // Formun içindeki Dosyaları anlık olarak alıp UseState içindeki değerlere kaydededen
+   //  Bu state alıp veri tabanına kaydedeceğiz
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+
+  }
+
   return (
-    <div>AddProduct</div>
+    <div className={styles.product}>
+      <h2>Add New Product</h2>
+      <Card cardClass={styles.card}>
+        <form>
+          <label>Product Name:</label>
+          <input 
+            type='text'
+            placeholder='Product Name'
+            required
+            name='name'
+            value={product.name}
+            onChange={(e)=>handleInputChange(e)}            
+          />          
+          <label>Product Image:</label>
+          <Card></Card>
+        </form>
+
+      </Card>
+    </div>
   )
 }
 
