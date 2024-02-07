703b8b388f4aecc183ef680c495aa967126de6a7
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-07 22:57:58 +0300

    Add Product page created

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -1,5 +1,7 @@
-//// admin panelindeki add product componenti
+// admin panelindeki add product componenti
 import React from 'react'
+import styles from "./AddProduct.module.scss"
+
 
 const AddProduct = () => {
   return (
