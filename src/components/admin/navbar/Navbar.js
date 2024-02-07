0e38d0ec739a0f4a889a6c5023dd501e555479dd
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-07 21:58:54 +0300

    Username  Icon  added to LeftNavbar

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -1,9 +1,23 @@
-////admin panelindeki sol navbar
+//Left Navbar in admin panel
 import React from 'react'
+import styles from "./Navbar.module.scss"
+import { useSelector } from 'react-redux'
+import { selectUserName } from '../../../redux/slice/authSlice'
+import { FaUserCircle } from 'react-icons/fa'
+
 
 const Navbar = () => {
+
+  const activeLink = ({isActive}) => (isActive ? `${styles.active}` : "")
+  const userName = useSelector
+
   return (
-    <div>Navbar</div>
+    <div className={styles.Navbar}>
+      <div className={styles.user}>      
+        <FaUserCircle size={40} color='#fff'/>
+        <h4>{userName}</h4>
+      </div>
+    </div>
   )
 }
 
