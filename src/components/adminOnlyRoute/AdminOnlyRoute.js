e8c8cb9014cdb1f6c72cdf26f32c38c496709658
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-07 16:15:17 +0300

    AdminOnlyRoute page generated

--- src/components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/components/adminOnlyRoute/AdminOnlyRoute.js
@@ -1,21 +1,35 @@
 //admin olmayıp da admin sayfasına erişmek isteyenler için permission denied sayfası
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectEmail } from '../../redux/slice/authSlice'
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectEmail } from "../../redux/slice/authSlice";
+import { Link } from "react-router-dom";
 
+const AdminOnlyRoute = ({ children }) => {
+  const userEmail = useSelector(selectEmail);
 
-const AdminOnlyRoute = () => {
-  return (
-    <div>AdminOnlyRoute</div>
-  )
-}
+  if (userEmail === "user1@gmail.com") {
+    return children;
+  } else {
+    return (
+    <section style={{height:"80vh"}}>
+      <div className="container">
+        <h2>Permission Denied</h2>
+        <p>This page can only be view by an Admin User </p>
+        <br/>
+        <Link to="/">
+          <button className="--btn">&larr; Bact to Home</button>
+        </Link>
+      </div>
+    </section>
+  )}
+};
 
-export const AdminOnlyLink = ({children}) => {
-  const userEmail = useSelector(selectEmail)
+export const AdminOnlyLink = ({ children }) => {
+  const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "user1@gmail.com"){
-    return children
-  } else return null
-}
+  if (userEmail === "user1@gmail.com") {
+    return children;
+  } else return null;
+};
 
-export default AdminOnlyRoute
+export default AdminOnlyRoute;
