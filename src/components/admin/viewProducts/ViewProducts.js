e1786c4c73ba82f8320f59d7ea335a26786e91d1
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-12 08:45:06 +0300

    deleteProduct function applied

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,18 +1,19 @@
 // The page we seen all the products
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
-import { db } from '../../../firebase/config'
+import { db, storage } from '../../../firebase/config'
 import Loader from '../../loader/Loader'
 import styles from "./ViewProducts.module.scss"
 import { Link } from 'react-router-dom'
 import { FaEdit, FaTrashAlt } from 'react-icons/fa'
+import { deleteObject, ref } from 'firebase/storage'
 
 
 const ViewProducts = () => {
 
-  const[products, setProducts] = useState([])     // React.useState([]) 
-  const[isLoading,setIsLoading] = useState([])     // React.useState([]) 
+  const[products, setProducts] = React.useState([])     //  useState([])
+  const[isLoading,setIsLoading] = React.useState([])     // 
 
   // In order to get data from DB automatically when the page open
   useEffect(()=>{
@@ -49,6 +50,42 @@ const ViewProducts = () => {
     }
     catch(error){
       setIsLoading(false)
+      toast.error(error.message)
+    }
+  }
+
+  const confirmDelete = (id,imageURL) => {
+    Notiflix.Confirm.show(
+      'Delete Product!!!',
+      'You are about to delete this product?',
+      'Delete',
+      'Cancel',
+      function okCb(){
+        deleteProduct(id,imageURL)
+      },
+      function cancelCb(){
+
+      },
+      {
+        width:"320px",
+        borderRadius:"3px",
+        titleColor:"orangered",
+        okButtonBackground:"orangered",
+        cssAnimationStyle: "zoom"
+      }
+    )
+  }
+
+  const deleteProduct = async(id,imageURL) => {
+    try {
+      await deleteDoc(doc(db,"products",id))
+
+      const storageRef = ref(storage,imageURL)
+      await deleteObject(storageRef)
+      
+      toast.success("Product deleted successfully")
+    }
+    catch(error){
       toast.error(error.message)
     }
   }
