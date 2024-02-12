157e2190c5b66c43c56c7c14d530b04147b6f61d
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-12 13:45:01 +0300

    Edit products  applied in ViewProducts

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         <ul>
           <li><NavLink to="/admin/home" className={activeLink}>Home</NavLink></li>
           <li> <NavLink to="/admin/all-products" className={activeLink}>All Products</NavLink></li>
-          <li> <NavLink to="/admin/add-product" className={activeLink}>Add Product</NavLink></li>
+          <li> <NavLink to="/admin/add-product/ADD" className={activeLink}>Add Product</NavLink></li>
           <li> <NavLink to="/admin/orders" className={activeLink}>Orders</NavLink></li>
         </ul>
       </nav>
