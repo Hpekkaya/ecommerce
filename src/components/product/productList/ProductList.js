25e64d90680351cb62eef57b3a7ec1b867a1428f
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-15 21:53:52 +0300

    Sortby products is applied to ProductLise Component

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -19,6 +19,17 @@ const ProductList = () => {
           <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
           <p><b>10</b> Products found</p>
         </div>
+        
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select name="category">
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest-to-Highest </option>
+            <option value="highest-price">Highest-to-Lowest </option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
 
       </div>
     </div>
