2b50c4813a33eb841335f1c9554a5460d52aacea
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-15 21:44:41 +0300

    Grid Structure is created

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,6 +1,10 @@
-// home sayfasındaki ürünler hakkında gösterim, arama, sıralama yapabildiğiniz kısımla beraber tüm ürün kartlarının sergilendiği yer. ürün kartları (productıtem) adlı komponentde işlenecektir.
-import React, { useState } from 'react'
+// It is the place where all product cards are displayed, along with the section where you can view, search and sort the products on the home page. 
+// Product cards will be processed in the component named (productitem).
 
+import React, { useState } from 'react'
+import styles from "./ProductList.module.scss";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
 
 const ProductList = () => {
 
@@ -8,7 +12,16 @@ const ProductList = () => {
   const [search,setSearch] = useState("")
 
   return (
-    <div>ProductList</div>
+    <div className={styles["product-list"]} id="products">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill size={22} color="orangered" onClick={() => setGrid(true)}/>
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+          <p><b>10</b> Products found</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
