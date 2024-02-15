3d9c0d7f5249c67ea667de073d624dbd9adf3ba7
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-15 23:27:22 +0300

    shortenText function is created

--- src/components/product/productItem/ProductItem.js
+++ src/components/product/productItem/ProductItem.js
@@ -2,6 +2,17 @@
 import React from 'react'
 
 const ProductItem = ({product,grid,id,name,price,desc,imageURL}) => {
+
+  // Shortens text over a certain length to create appearance standards
+  const shortenText = (text,n) => {
+    if(text.length > n) {
+      const shortenedText = text.substring(0,n).concat("...")
+      return shortenedText;
+    }
+    else {
+      return text;
+    }
+  }
   return (
     <div>
       
