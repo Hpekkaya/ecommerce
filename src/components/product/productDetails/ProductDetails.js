8505dd4a3bb7167c057e21842cddceb48be7d72d
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-16 17:39:50 +0300

    useEffect function applied in ProductDetails component

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,9 +1,29 @@
 // The product details component that appears when we click on the products on the home page
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from "./ProductDetails.module.scss";
+import { useParams } from 'react-router-dom';
+import useFetchDocument from '../../../customHooks/useFetchDocument';
+import spinnerImg from "../../../assets/spinner.gif"
 
 const ProductDetails = () => {
+
+  // To get the ID in the link above
+  const {id} = useParams()
+
+  // To keep product related details
+  const [product, setProduct ] = useState(null)
+
+  // 
+  const document = useFetchDocument("product", id)
+
+  useEffect(()=>{
+    setProduct(document)
+  },[document])
+
   return (
-    <div>ProductDetails</div>
+    <div>
+      {JSON.stringify(document)}
+    </div>
   )
 }
 
