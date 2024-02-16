94f90eda4bc516411d052c58f16b4bb9101003ee
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-16 18:00:38 +0300

    ProductDetails component is improved

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,7 +1,7 @@
 // The product details component that appears when we click on the products on the home page
 import React, { useEffect, useState } from 'react'
 import styles from "./ProductDetails.module.scss";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import useFetchDocument from '../../../customHooks/useFetchDocument';
 import spinnerImg from "../../../assets/spinner.gif"
 
@@ -21,9 +21,32 @@ const ProductDetails = () => {
   },[document])
 
   return (
-    <div>
-      {JSON.stringify(document)}
-    </div>
+    <section>
+      <div className={`container ${styles.product}`}>
+        <h2>Product Details</h2>
+        <Link to="/#products">&larr; Back to Products</Link>
+      </div>
+      {products === ? (
+        <img src={spinnerImg} alt ="Loading..." style={{width:"50px"}}/>
+      ):(
+        <>
+          <div className={styles.details}>
+            <div className={styles.img}>
+              <img src={product.imgURL} alt ="{product.name}"/>
+            </div>
+            <div className={styles.content}>
+              <h3>{product.name}</h3> 
+              <p className={styles.price}>{` $${product.price}`}</p>   
+              <p>{product.desc}</p>  
+              <p><b>SKU</b>{product.id}</p>
+              <p><b>Brand</b>{product.brand}</p>
+              <div></div>
+
+            </div>
+          </div>
+        </>
+      )}
+    </section>         
   )
 }
 
