c64f1758e4c68e4779f2f2d36ddf9951d8b4dc79
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-16 16:54:54 +0300

    ProductDetails component is created

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,4 +1,4 @@
-//// home sayfasındaki ürünlere tıkladığımızda çıkan product details componenti
+// The product details component that appears when we click on the products on the home page
 import React from 'react'
 
 const ProductDetails = () => {
